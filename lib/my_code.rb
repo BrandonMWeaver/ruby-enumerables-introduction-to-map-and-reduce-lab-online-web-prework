0940d3689e86bea0b6033d98e31440cabc5d8a7e
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 20:14:05 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -40,6 +40,15 @@ def map_to_square(source_array)
   return temp_array
 end
 
-def reduce_to_total(source_array, starting_point)
+def reduce_to_total(source_array, starting_point = 0)
+  temp_array = source_array
   
+  index = 0
+  while index < temp_array.size do
+    temp_array[index] += starting_point
+    starting_point += temp_array[index]
+    index += 1
+  end
+  
+  return temp_array
 end
