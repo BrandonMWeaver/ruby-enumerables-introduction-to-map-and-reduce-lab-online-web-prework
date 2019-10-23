01f0a532c5a495369d1a75b0202658832e5914d5
Brandon Weaver <a7xncob@gmail.com>
2019-10-23 20:17:44 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -52,5 +52,14 @@ def reduce_to_total(source_array, starting_point = 0)
 end
 
 def reduce_to_all_true(source_array)
+  temp_array = []
+  
+  index = 0
+  while index < source_array.size do
+    if source_array[index] == true
+      temp_array << source_array[index]
+    end
+    index += 1
+  end
   
 end
